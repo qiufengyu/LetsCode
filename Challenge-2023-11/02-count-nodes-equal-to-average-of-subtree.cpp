676f31d759a612a3d0e2e9ee30382ba402496e9f
Fengyu Qiu <mrqiufengyu@gmail.com>
2023-11-06 23:16:59 +0800

    Nov 6th, 2023

---
+++ Challenge-2023-11/02-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,24 @@
+﻿#include "../header.h"
+
+class Solution {
+public:
+    int averageOfSubtree(TreeNode* root) {
+        int res = 0;
+        postOrder(root, res);
+        return res;
+    }
+
+    pair<int, int> postOrder(TreeNode* node, int& res) {
+        if (node) {
+            auto l = postOrder(node->left, res);
+            auto r = postOrder(node->right, res);
+            int s = l.first + r.first + node->val;
+            int c = l.second + r.second + 1;
+            if (s / c == node->val) {
+                ++res;
+            }
+            return {s, c};
+        }
+        return {0, 0};
+    }
+};
---
+++ Challenge-2023-11/03-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,19 @@
+﻿#include "../header.h"
+
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> res;
+        int current = 1;
+        for (int i = 0; i < target.size(); i++) {
+            res.push_back("Push");
+            while (current <= n && target[i] != current) {
+                res.push_back("Pop");
+                ++current;
+                res.push_back("Push");
+            }
+            ++current;
+        }
+        return res;
+    }
+};
