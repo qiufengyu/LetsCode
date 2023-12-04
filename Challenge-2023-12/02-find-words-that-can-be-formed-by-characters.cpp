899d9ee12baa5ac2b24778c421b411c00a98f583
Fengyu Qiu <mrqiufengyu@gmail.com>
2023-12-05 01:41:07 +0800

    December 4th, 2023

---
+++ Challenge-2023-12/02-find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,28 @@
+﻿#include "../header.h"
+
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        unordered_map<char, int> cm;
+        int res = 0;
+        for (int i = 0; i < chars.size(); i++) {
+           cm[chars[i]]++;
+        }
+        for (int i = 0; i < words.size(); i++) {
+            string w = words[i];
+            unordered_map<char, int> wm;
+            int flag = 1;
+            for (int j = 0; j < w.size(); j++) {
+                wm[w[j]] ++;
+            }
+            for (auto const p: wm) {
+                if (cm.count(p.first) == 0 || cm[p.first] < p.second) {
+                    flag = 0;
+                    break;
+                }
+            }
+            res += (flag * w.size());
+        }
+        return res;
+    }
+};
