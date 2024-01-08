1960c0a1b3d72450b9049a8767fdd4c659b685f1
Fengyu Qiu <mrqiufengyu@gmail.com>
2024-01-08 23:24:09 +0800

    Jan 8, 2024

---
+++ Challenge-2024-01/02-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,20 @@
+﻿#include "../header.h"
+
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        unordered_map<int, int> m;
+        int n = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+            n = max(n, m[nums[i]]);
+        }
+        vector<vector<int>> res(n, vector<int>());
+        for (auto const& p: m) {
+            for (int i = 0; i < p.second; i++) {
+                res[i].push_back(p.first);
+            }
+        }
+        return res;
+    }
+};
---
+++ Challenge-2024-01/04-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,19 @@
+﻿#include "../header.h"
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        int res = 0;
+        for (auto const& p: m) {
+            if (p.second == 1) {
+                return -1;
+            }
+            res += ((p.second + 2) / 3);
+        }
+        return res;
+    }
+};
